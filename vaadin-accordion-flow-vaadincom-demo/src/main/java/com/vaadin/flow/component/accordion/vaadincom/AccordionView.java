033f001be4671dec2f39dfcf482b68a42bb0bde4
Sayo Oladeji <oladejioluwasayo@gmail.com>
2019-01-28 20:44:49 +0200

    MVP (#3)

---
+++ vaadin-accordion-flow-vaadincom-demo/src/main/java/com/vaadin/flow/component/accordion/vaadincom/AccordionView.java
@@ -0,0 +1,25 @@
+package com.vaadin.flow.component.accordion.vaadincom;
+
+import com.vaadin.flow.demo.DemoView;
+import com.vaadin.flow.router.Route;
+
+@SuppressWarnings("serial")
+@Route("vaadin-accordion")
+public class AccordionView extends DemoView {
+
+    @Override
+    protected void initView() {
+        basicAccordion();
+    }
+
+    private void basicAccordion() {
+        // begin-source-example
+        // source-example-heading: Basic Accordion
+//        Accordion accordion = new Accordion();
+
+
+        // end-source-example
+
+//        addCard("Basic Accordion", accordion);
+    }
+}
