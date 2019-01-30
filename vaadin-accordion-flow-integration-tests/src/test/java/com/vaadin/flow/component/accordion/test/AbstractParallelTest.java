91266be5ecb25e3a14718f1ea8b13a79baee5236
Sayo Oladeji <sayo@vaadin.com>
2019-01-30 19:30:25 +0200

    Use Beta webjar

--- vaadin-accordion-flow-integration-tests/src/test/java/com/vaadin/flow/component/accordion/test/AbstractParallelTest.java
+++ vaadin-accordion-flow-integration-tests/src/test/java/com/vaadin/flow/component/accordion/test/AbstractParallelTest.java
@@ -70,12 +70,9 @@ public abstract class AbstractParallelTest extends ParallelTest {
 
     @BrowserConfiguration
     public List<DesiredCapabilities> getBrowserConfiguration() {
-        // TODO(oluwasayo): Re-enable after webjar release and remove useBower from .travis.yml
-//        DesiredCapabilities ie11Windows8_1 = BrowserUtil.ie11();
-//        ie11Windows8_1.setPlatform(Platform.WIN8_1);
-//        return Arrays.asList(ie11Windows8_1, BrowserUtil.firefox(),
-//                BrowserUtil.chrome(), BrowserUtil.edge(), BrowserUtil.safari());
-        return Arrays.asList(BrowserUtil.firefox(),
+        DesiredCapabilities ie11Windows8_1 = BrowserUtil.ie11();
+        ie11Windows8_1.setPlatform(Platform.WIN8_1);
+        return Arrays.asList(ie11Windows8_1, BrowserUtil.firefox(),
                 BrowserUtil.chrome(), BrowserUtil.edge(), BrowserUtil.safari());
     }
 
